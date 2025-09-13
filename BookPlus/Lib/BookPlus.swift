cd46043def0dafd8e4abe8372e1030b157b3dee7
CaioArg <caioarg@outlook.com>
2025-09-13 07:29:16 -0300

    simplificando api

--- BookPlus/Lib/BookPlus.swift
+++ BookPlus/Lib/BookPlus.swift
@@ -1,19 +1,18 @@
 import ARKit
 
 public class BookPlus: NSObject, ARSCNViewDelegate {
-    private let pages: [Page]!
-    private let sceneView: ARSCNView!
-    private let configuration: ARImageTrackingConfiguration!
+    private let pages: [Page]
+    private let sceneView: ARSCNView
+    private let configuration = ARImageTrackingConfiguration()
 
-    public init(with pages: [Page], for sceneView: ARSCNView, and configuration: ARImageTrackingConfiguration) {
+    public init(with pages: [Page], for sceneView: ARSCNView) {
         self.pages = pages
         self.sceneView = sceneView
-        self.configuration = configuration
 
         self.configuration.trackingImages = Set(self.pages.map { $0.pageImage })
         self.configuration.maximumNumberOfTrackedImages = 1
     }
-
+    
     public func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
         guard let anchor = anchor as? ARImageAnchor else { return nil }
         guard let renderStrategy = pages.first(where: { $0.pageImage == anchor.referenceImage })?.renderStrategy else { return nil }
@@ -24,5 +23,13 @@ public class BookPlus: NSObject, ARSCNViewDelegate {
         guard let imageAnchor = anchor as? ARImageAnchor else { return }
         guard !node.isHidden else { return }
         sceneView.session.remove(anchor: imageAnchor)
+    }
+
+    public func run() {
+        self.sceneView.session.run(self.configuration)
+    }
+
+    public func pause() {
+        self.sceneView.session.pause()
     }
 }
--- BookPlus/ViewController/SceneViewController.swift
+++ BookPlus/ViewController/SceneViewController.swift
@@ -2,27 +2,21 @@ import ARKit
 
 class SceneViewController: UIViewController {
     @IBOutlet weak var sceneView: ARSCNView!
-
     var pages: [Page]!
-
-    private var configuration: ARImageTrackingConfiguration!
     private var bookPlus: BookPlus!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        sceneView.scene = SCNScene()
-        configuration = ARImageTrackingConfiguration()
-        bookPlus = BookPlus(with: pages, for: sceneView, and: configuration)
-        sceneView.delegate = bookPlus
+        self.bookPlus = BookPlus(with: pages, for: sceneView)
     }
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        sceneView.session.run(configuration)
+        self.bookPlus.run()
     }
 
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-        sceneView.session.pause()
+        self.bookPlus.pause()
     }
 }
